d4d5bff5d9d5cc17ced26aaeafb123e6ac0873f5
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-17 20:34:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,13 +9,12 @@ reversed.join(" ")
   
 end 
 
-def reverse_each_word(str)
-  arr = str.split()
-  reversed = []
-  arr.each do |word|
-    reversed << word.reverse 
-  end 
-  
-reversed.join(" ")
-  
-end 
+#def reverse_each_word(str)
+#  arr = str.split()
+#  reversed = []
+#  arr.each do |word|
+#    reversed << word.reverse 
+#  end 
+#  
+#reversed.join(" ")
+#end 
