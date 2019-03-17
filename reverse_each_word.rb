ec3a2fcc9428acd7115b9d9ae3ff5987deaa4ff4
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-17 20:33:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(str)
   arr = str.split()
-  
-  arr.collect do |word|
-    word.reverse 
+  reversed = []
+  arr.each do |word|
+    reversed << word.reverse 
   end 
   
-  arr.join()
+reversed.join
   
 end 
